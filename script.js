9a3ebe9e9aea349767541c7633e9c2011589581c
PWEB_A_19-2083 <72341703+AriaDesta2083@users.noreply.github.com>
2020-11-12 21:37:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+var slideIndex = 0;
+showSlides();
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1} 
+    for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 4000);
+}
+
 function sayHello(){
     yourName = document.getElementById("username").value;
     if (yourName == "" || !yourName)
@@ -21,60 +35,3 @@ function mypassword() {
       x.type = "password";
     }
 }
-
-
-const initBg = (autoplay = true) => {
-    const bgImgsNames = ['kebun.jpg', 'Gandum.jpg', 'tani.jpg' , 'Teknologi.jpg'];
-    const bgImgs = bgImgsNames
-
-    $.backstretch(bgImgs, {duration: 3000, fade: 500});
-
-    if(!autoplay) {
-      $.backstretch('pause');  
-    }    
-}
-
-const setBg = id => {
-    $.backstretch('show', id);
-}
-
-const setBgOverlay = () => {
-    const windowWidth = window.innerWidth;
-    const bgHeight = $('body').height();
-    const tmBgLeft = $('.tm-bg-left');
-
-    $('.tm-bg').height(bgHeight);
-
-    if(windowWidth > 768) {
-        tmBgLeft.css('border-left', `0`)
-                .css('border-top', `${bgHeight}px solid transparent`);                
-    } else {
-        tmBgLeft.css('border-left', `${windowWidth}px solid transparent`)
-                .css('border-top', `0`);
-    }
-}
-
-$(document).ready(function () {
-    const autoplayBg = true;	// set Auto Play for Background Images
-    initBg(autoplayBg);    
-    setBgOverlay();
-
-    const bgControl = $('.tm-bg-control');            
-    bgControl.click(function() {
-        bgControl.removeClass('active');
-        $(this).addClass('active');
-        const id = $(this).data('id');                
-        setBg(id);
-    });
-
-    $(window).on("backstretch.after", function (e, instance, index) {        
-        const bgControl = $('.tm-bg-control');
-        bgControl.removeClass('active');
-        const current = $(".tm-bg-controls-wrapper").find(`[data-id=${index}]`);        
-        current.addClass('active');
-    });
-
-    $(window).resize(function() {
-        setBgOverlay();
-    });
-});
